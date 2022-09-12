a921c17fc5556f2c92ac9f029a51487d79ff48fc
dguarisma <david.g@selia.co>
2022-09-12 12:05:54 -0400

    add Category

--- src/pages/apps/categories/add-category.tsx
+++ src/pages/apps/categories/add-category.tsx
@@ -2,36 +2,15 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 // material-ui
-import { Button, Grid, InputLabel, MenuItem, Stack, TextField, Typography } from '@mui/material';
+import { Button, Grid, InputLabel, MenuItem, Stack, TextField, Typography, FormControlLabel, Switch } from '@mui/material';
 
 // project import
 import MainCard from 'components/MainCard';
-
-// assets
-import { UploadOutlined } from '@ant-design/icons';
 
 // ==============================|| ADD NEW PRODUCT - MAIN ||============================== //
 
 function AddNewProduct() {
   const history = useNavigate();
-  const prices = [
-    {
-      value: '1',
-      label: '$ 100'
-    },
-    {
-      value: '2',
-      label: '$ 200'
-    },
-    {
-      value: '3',
-      label: '$ 300'
-    },
-    {
-      value: '4',
-      label: '$ 400'
-    }
-  ];
 
   const quantities = [
     {
@@ -48,31 +27,10 @@ function AddNewProduct() {
     }
   ];
 
-  const statuss = [
-    {
-      value: 'in stock',
-      label: 'In Stock'
-    },
-    {
-      value: 'out of stock',
-      label: 'Out of Stock'
-    }
-  ];
-
   const [quantity, setQuantity] = useState('one');
-  const [price, setPrice] = useState('1');
-  const [status, setStatus] = useState('in stock');
-
-  const handlePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPrice(event.target.value);
-  };
 
   const handleQuantity = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuantity(event.target.value);
-  };
-
-  const handleStatus = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setStatus(event.target.value);
   };
 
   const handleCancel = () => {
@@ -87,27 +45,72 @@ function AddNewProduct() {
             <MainCard>
               <Grid container spacing={1} direction="column">
                 <Grid item xs={12}>
-                  <Typography variant="subtitle1">Categoria</Typography>
-                  <InputLabel sx={{ mb: 1, mt: 2, opacity: 0.5 }}>Product Name</InputLabel>
-                  <TextField sx={{ '& .MuiOutlinedInput-input': { opacity: 0.5 } }} placeholder="Enter product name" fullWidth />
+                  <Typography variant="h5" component="div" sx={{ mb: 3 }}>
+                    Categoria
+                  </Typography>
+                  <Grid container spacing={2} alignItems="center">
+                    <Grid item xs={12} sm={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                      <InputLabel>Nombre de Categoria</InputLabel>
+                    </Grid>
+                    <Grid item xs={12} sm={8}>
+                      <TextField sx={{ '& .MuiOutlinedInput-input': { opacity: 0.5 } }} placeholder="Ingresar Nombre Categoria" fullWidth />
+                    </Grid>
+                    <Grid item xs={12} sm={3} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                      <InputLabel>Estado</InputLabel>
+                    </Grid>
+                    <Grid item xs={12} sm={9}>
+                      <FormControlLabel control={<Switch sx={{ mt: 0 }} />} label="" labelPlacement="top" />
+                    </Grid>
+                  </Grid>
                 </Grid>
                 <Grid item xs={12}>
-                  <InputLabel sx={{ mb: 1, opacity: 0.5 }}>Product Description</InputLabel>
-                  <TextField sx={{ '& .MuiOutlinedInput-input': { opacity: 0.5 } }} placeholder="Enter your email" fullWidth />
+                  <Stack direction="row" spacing={2} justifyContent="right" alignItems="center" sx={{ mt: 6 }}>
+                    <Button variant="outlined" color="secondary" onClick={handleCancel}>
+                      Cancel
+                    </Button>
+                    <Button variant="contained" sx={{ textTransform: 'none' }}>
+                      Add
+                    </Button>
+                  </Stack>
+                </Grid>
+              </Grid>
+            </MainCard>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <MainCard>
+              <Grid container spacing={1} direction="column">
+                <Grid item xs={12}>
+                  <Typography variant="h5" component="div" sx={{ mb: 3 }}>
+                    Categoria 2
+                  </Typography>
+                  <Grid container spacing={2} alignItems="center">
+                    <Grid item xs={12} sm={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                      <InputLabel>Nombre de Categoria 2</InputLabel>
+                    </Grid>
+                    <Grid item xs={12} sm={8}>
+                      <TextField
+                        sx={{ '& .MuiOutlinedInput-input': { opacity: 0.5 } }}
+                        placeholder="Ingresar Nombre Categoria 2"
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                      <InputLabel>Estado</InputLabel>
+                    </Grid>
+                    <Grid item xs={12} sm={8}>
+                      <FormControlLabel control={<Switch sx={{ mt: 0 }} />} label="" labelPlacement="top" />
+                    </Grid>
+                  </Grid>
                 </Grid>
                 <Grid item xs={12}>
-                  <InputLabel sx={{ mb: 1, opacity: 0.5 }}>Category</InputLabel>
-                  <TextField sx={{ '& .MuiOutlinedInput-input': { opacity: 0.5 } }} placeholder="Enter your category" fullWidth />
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel sx={{ mb: 1, opacity: 0.5 }}>Price</InputLabel>
-                  <TextField placeholder="Select Price" fullWidth select value={price} onChange={handlePrice}>
-                    {prices.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </TextField>
+                  <Stack direction="row" spacing={2} justifyContent="right" alignItems="center" sx={{ mt: 6 }}>
+                    <Button variant="outlined" color="secondary" onClick={handleCancel}>
+                      Cancel
+                    </Button>
+                    <Button variant="contained" sx={{ textTransform: 'none' }}>
+                      Add
+                    </Button>
+                  </Stack>
                 </Grid>
               </Grid>
             </MainCard>
@@ -116,35 +119,51 @@ function AddNewProduct() {
             <MainCard>
               <Grid container direction="column" spacing={2}>
                 <Grid item xs={12}>
-                  <InputLabel sx={{ mb: 1, opacity: 0.5 }}>Qty</InputLabel>
-                  <TextField placeholder="Select quantity" fullWidth select value={quantity} onChange={handleQuantity}>
-                    {quantities.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                </Grid>
-                <Grid item xs={12}>
-                  <InputLabel sx={{ mb: 1, opacity: 0.5 }}>Status</InputLabel>
-                  <TextField placeholder="Select status" fullWidth select value={status} onChange={handleStatus}>
-                    {statuss.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography color="error.main">
-                    *{' '}
-                    <Typography component="span" color="textSecondary">
-                      Recommanded resolution is 640*640 with file size
-                    </Typography>
+                  <Typography variant="h5" component="div" sx={{ mb: 3 }}>
+                    Categoria 3
                   </Typography>
-                  <Button variant="outlined" color="secondary" startIcon={<UploadOutlined />} sx={{ mt: 1, textTransform: 'none' }}>
-                    Click to Upload
-                  </Button>
+                  <Grid container spacing={2} alignItems="center">
+                    <Grid item xs={12} sm={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                      <InputLabel>Nombre de Categoria 3</InputLabel>
+                    </Grid>
+                    <Grid item xs={12} sm={8}>
+                      <TextField
+                        sx={{ '& .MuiOutlinedInput-input': { opacity: 0.5 } }}
+                        placeholder="Ingresar Nombre Categoria 3"
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                      <InputLabel>Categoria 1</InputLabel>
+                    </Grid>
+                    <Grid item xs={12} sm={8}>
+                      <TextField placeholder="Select quantity" fullWidth select value={quantity} onChange={handleQuantity}>
+                        {quantities.map((option) => (
+                          <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </Grid>
+                    <Grid item xs={12} sm={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                      <InputLabel>Categoria 2</InputLabel>
+                    </Grid>
+                    <Grid item xs={12} sm={8}>
+                      <TextField placeholder="Select quantity" fullWidth select value={quantity} onChange={handleQuantity}>
+                        {quantities.map((option) => (
+                          <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </Grid>
+                    <Grid item xs={12} sm={4} sx={{ pt: { xs: 2, sm: '0 !important' } }}>
+                      <InputLabel>Estado</InputLabel>
+                    </Grid>
+                    <Grid item xs={12} sm={8}>
+                      <FormControlLabel control={<Switch sx={{ mt: 0 }} />} label="" labelPlacement="top" />
+                    </Grid>
+                  </Grid>
                 </Grid>
                 <Grid item xs={12}>
                   <Stack direction="row" spacing={2} justifyContent="right" alignItems="center" sx={{ mt: 6 }}>
@@ -152,7 +171,7 @@ function AddNewProduct() {
                       Cancel
                     </Button>
                     <Button variant="contained" sx={{ textTransform: 'none' }}>
-                      Add new Product
+                      Add
                     </Button>
                   </Stack>
                 </Grid>
