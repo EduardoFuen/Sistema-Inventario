59b38fd5d26d0ac06485f7e1b01fdfe69c0172f3
dguarisma <david.g@selia.co>
2022-09-22 06:41:50 -0400

    Inventory

--- src/pages/apps/inventory/inventory-list.tsx
+++ src/pages/apps/inventory/inventory-list.tsx
@@ -4,10 +4,9 @@ import { useMemo, Fragment } from 'react';
 
 // material-ui
 import { alpha, useTheme } from '@mui/material/styles';
-import { Chip, Stack, Table, TableBody, TableCell, TableHead, TableRow, useMediaQuery } from '@mui/material';
+import { Stack, Table, TableBody, TableCell, TableHead, TableRow, useMediaQuery, Typography } from '@mui/material';
 
 // third-party
-import NumberFormat from 'react-number-format';
 import { useFilters, useExpanded, useGlobalFilter, useRowSelect, useSortBy, useTable, usePagination, Column } from 'react-table';
 
 // project import
@@ -152,44 +151,145 @@ const InventoryList = () => {
       },
       {
         Header: 'SKU',
-        accessor: 'warehouse'
+        accessor: 'sku'
       },
       {
         Header: 'EAN',
-        accessor: 'subtotal',
-        className: 'cell-right',
-        Cell: ({ value }: any) => <NumberFormat value={value} displayType="text" thousandSeparator prefix="$" />
+        accessor: 'ean'
       },
       {
-        Header: 'IVA',
-        accessor: 'tax',
-        className: 'cell-right',
-        Cell: ({ value }: any) => <NumberFormat value={value} displayType="text" thousandSeparator prefix="$" />
+        Header: 'Producto',
+        accessor: 'nameProduct'
       },
       {
-        Header: 'Total',
-        accessor: 'total',
-        className: 'cell-right',
-        Cell: ({ value }: any) => <NumberFormat value={value} displayType="text" thousandSeparator prefix="$" />
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Unidades</Typography>
+              <Typography variant="caption" color="textSecondary">
+                inventario Inicial
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'initialInvUnd'
       },
       {
-        Header: 'Estado',
-        accessor: 'status',
-        Cell: ({ value }: any) => {
-          switch (value) {
-            case 'Refunded':
-              return <Chip color="error" label="Cancelled" size="small" variant="light" />;
-            case 'Completed':
-              return <Chip color="success" label="Completed" size="small" variant="light" />;
-            case 'Cancelled':
-              return <Chip color="secondary" label="Closed" size="small" variant="light" />;
-            case 'Processing':
-              return <Chip color="info" label="Processing" size="small" variant="light" />;
-            case 'Delivered':
-            default:
-              return <Chip color="warning" label="New" size="small" variant="light" />;
-          }
-        }
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Costo</Typography>
+              <Typography variant="caption" color="textSecondary">
+                inventario Inicial
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'initialInvCosto'
+      },
+      {
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Unidades</Typography>
+              <Typography variant="caption" color="textSecondary">
+                Compras
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'purchaseInvUnd'
+      },
+      {
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Costo</Typography>
+              <Typography variant="caption" color="textSecondary">
+                Compras
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'purchaseInvCosto'
+      },
+      {
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Unidades</Typography>
+              <Typography variant="caption" color="textSecondary">
+                Inventario Disponible
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'availableInvUnd'
+      },
+      {
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Costo</Typography>
+              <Typography variant="caption" color="textSecondary">
+                Inventario Disponible
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'availableInvCosto'
+      },
+      {
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Unidades</Typography>
+              <Typography variant="caption" color="textSecondary">
+                Ventas
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'salesInvUnd'
+      },
+      {
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Costo</Typography>
+              <Typography variant="caption" color="textSecondary">
+                Ventas
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'salesInvCosto'
+      },
+      {
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Unidades</Typography>
+              <Typography variant="caption" color="textSecondary">
+                Inventario Final
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'endInvUnd'
+      },
+      {
+        Header: () => (
+          <Stack direction="row" spacing={1.5} alignItems="center" sx={{ textAlign: 'center' }}>
+            <Stack spacing={0}>
+              <Typography variant="subtitle1">Costo</Typography>
+              <Typography variant="caption" color="textSecondary">
+                Inventario Final
+              </Typography>
+            </Stack>
+          </Stack>
+        ),
+        accessor: 'endInvCosto'
       }
     ],
     // eslint-disable-next-line react-hooks/exhaustive-deps
