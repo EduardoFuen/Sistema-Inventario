797a9a9ebb37b450188f3a1d3494e54aa520a337
dguarisma <david.g@selia.co>
2022-10-19 12:25:46 -0400

    updata table

--- src/pages/apps/inventory/inventory-list.tsx
+++ src/pages/apps/inventory/inventory-list.tsx
@@ -1,140 +1,14 @@
-import { useMemo, Fragment } from 'react';
-
-// import PSPDFKit from "./PSPDFKit";
+import { useMemo } from 'react';
 
 // material-ui
-import { alpha, useTheme } from '@mui/material/styles';
-import { Stack, Table, TableBody, TableCell, TableHead, TableRow, useMediaQuery, Typography } from '@mui/material';
-
-// third-party
-import { useFilters, useExpanded, useGlobalFilter, useRowSelect, useSortBy, useTable, usePagination, Column } from 'react-table';
+import { useTheme } from '@mui/material/styles';
+import { Stack, Typography } from '@mui/material';
 
 // project import
 import MainCard from 'components/MainCard';
 import ScrollX from 'components/ScrollX';
+import ReactTable from 'components/ReactTable';
 import { useSelector } from 'store';
-
-import Export from 'components/ExportToFile';
-import { renderFilterTypes, GlobalFilter } from 'utils/react-table';
-import { HeaderSort, SortingSelect, TablePagination } from 'components/third-party/ReactTable';
-
-// ==============================|| REACT TABLE ||============================== //
-
-interface Props {
-  columns: Column[];
-  data: [];
-  getHeaderProps: (column: any) => void;
-}
-
-function ReactTable({ columns, data, getHeaderProps }: Props) {
-  const theme = useTheme();
-  const matchDownSM = useMediaQuery(theme.breakpoints.down('sm'));
-
-  const filterTypes = useMemo(() => renderFilterTypes, []);
-  const sortBy = { id: 'create_order', desc: false };
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    prepareRow,
-    allColumns,
-    rows,
-    // @ts-ignore
-    page,
-    // @ts-ignore
-    gotoPage,
-    // @ts-ignore
-    setPageSize,
-    // @ts-ignore
-    state: { globalFilter, pageIndex, pageSize },
-    // @ts-ignore
-    preGlobalFilteredRows,
-    // @ts-ignore
-    setGlobalFilter,
-    // @ts-ignore
-    setSortBy
-  } = useTable(
-    {
-      columns,
-      data,
-      // @ts-ignore
-      filterTypes,
-      // @ts-ignore
-      initialState: { pageIndex: 0, pageSize: 10, sortBy: [sortBy] }
-    },
-    useGlobalFilter,
-    useFilters,
-    useSortBy,
-    useExpanded,
-    usePagination,
-    useRowSelect
-  );
-
-  return (
-    <>
-      <Stack spacing={3}>
-        <Stack
-          direction={matchDownSM ? 'column' : 'row'}
-          spacing={1}
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{ p: 3, pb: 0 }}
-        >
-          <GlobalFilter
-            preGlobalFilteredRows={preGlobalFilteredRows}
-            globalFilter={globalFilter}
-            setGlobalFilter={setGlobalFilter}
-            size="small"
-          />
-          <Export excelData={data} fileName="Purchase" />
-          <Stack direction={matchDownSM ? 'column' : 'row'} alignItems="center" spacing={1}>
-            <SortingSelect sortBy={sortBy.id} setSortBy={setSortBy} allColumns={allColumns} />
-          </Stack>
-        </Stack>
-
-        <Table {...getTableProps()}>
-          <TableHead>
-            {headerGroups.map((headerGroup) => (
-              <TableRow {...headerGroup.getHeaderGroupProps()} sx={{ '& > th:first-of-type': { width: '58px' } }}>
-                {headerGroup.headers.map((column: any) => (
-                  <TableCell {...column.getHeaderProps([{ className: column.className }, getHeaderProps(column)])}>
-                    <HeaderSort column={column} />
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableHead>
-          <TableBody {...getTableBodyProps()}>
-            {page.map((row: any, i: number) => {
-              prepareRow(row);
-              return (
-                <Fragment key={i}>
-                  <TableRow
-                    {...row.getRowProps()}
-                    onClick={() => {
-                      row.toggleRowSelected();
-                    }}
-                    sx={{ cursor: 'pointer', bgcolor: row.isSelected ? alpha(theme.palette.primary.lighter, 0.35) : 'inherit' }}
-                  >
-                    {row.cells.map((cell: any) => (
-                      <TableCell {...cell.getCellProps([{ className: cell.column.className }])}>{cell.render('Cell')}</TableCell>
-                    ))}
-                  </TableRow>
-                </Fragment>
-              );
-            })}
-            <TableRow sx={{ '&:hover': { bgcolor: 'transparent !important' } }}>
-              <TableCell sx={{ p: 2, py: 3 }} colSpan={9}>
-                <TablePagination gotoPage={gotoPage} rows={rows} setPageSize={setPageSize} pageSize={pageSize} pageIndex={pageIndex} />
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Stack>
-    </>
-  );
-}
 
 // ==============================|| INVENTORY - LIST VIEW ||============================== //
 
@@ -298,7 +172,15 @@ const InventoryList = () => {
   return (
     <MainCard content={false}>
       <ScrollX>
-        <ReactTable columns={columns} data={listPurchase as []} getHeaderProps={(column: any) => column.getSortByToggleProps()} />
+        <ReactTable
+          columns={columns}
+          data={listPurchase as []}
+          handleImport={() => {}}
+          handleAdd={() => {}}
+          TitleButton=""
+          FileName="ReporteInventario"
+          getHeaderProps={(column: any) => column.getSortByToggleProps()}
+        />
       </ScrollX>
     </MainCard>
   );
